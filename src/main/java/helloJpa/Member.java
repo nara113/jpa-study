e4a44568537ba79acd66a007f3d6e1546880b3da
sedovila <0554my@naver.com>
2021-12-23 03:10:33 +0900

    필드와 컬럼 매핑

--- src/main/java/helloJpa/Member.java
+++ src/main/java/helloJpa/Member.java
@@ -4,16 +4,40 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor //JPA에서 리플렉션을 사용하기 때문에 필요
+@NoArgsConstructor
 public class Member {
     @Id
     private Long id;
-    private String name;
+
+//    @Column(unique = true, length = 10) //DDL 생성기능
+//    private String name;
+
+    @Column(name = "name", updatable = false, nullable = true)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+    private LocalDateTime createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob    //매핑하는 필드 타입이 문자면 CLOB 매핑, 나머지는 BLOB 매핑
+    private String description;
+
+    @Transient
+    private String temp;
 }
