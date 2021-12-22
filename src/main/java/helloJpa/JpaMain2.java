abf84c30496b25720445356c1658be402b17562e
sedovila <0554my@naver.com>
2021-12-22 22:53:55 +0900

    영속성 컨텍스트

--- src/main/java/helloJpa/JpaMain2.java
+++ src/main/java/helloJpa/JpaMain2.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaMain {
+public class JpaMain2 {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -16,22 +16,17 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("hi");
+            //쓰기 지연
+            em.persist(new Member(1L, "A"));
+            em.persist(new Member(2L, "B"));
+            em.persist(new Member(3L, "B"));
+            em.persist(new Member(4L, "B"));
 
-            //영속
-            em.persist(member);
+            Member member = em.find(Member.class, 1L);
 
-            //1차 캐시에서 가져옴
-            Member member1 = em.find(Member.class, 100L);
-            Member member2 = em.find(Member.class, 100L);
+            //변경 감지
+            member.setName("zzz");
 
-            //true (REPEATABLE READ 격리 수준을 애플리케이션 차원에서 제공)
-            System.out.println(member1 == member2);
-
-            //커밋 시점에 SQL을 DB로 보낸다.
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/helloJpa/Member.java
+++ src/main/java/helloJpa/Member.java
@@ -1,5 +1,7 @@
 package helloJpa;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor //JPA에서 리플렉션을 사용하기 때문에 필요
 public class Member {
     @Id
     private Long id;
